270bdf7c29392272e4e21fc2402ba17c5236059d
dilesh <drj855@mun.ca>
2020-05-16 23:00:28 -0230

    hosting config left

--- basic_element_database/BasicElementDbContext.cs
+++ basic_element_database/BasicElementDbContext.cs
@@ -1,16 +1,19 @@
 ﻿using basic_element_database.Elements;
 using Microsoft.EntityFrameworkCore;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace basic_element_database
 {
     public class BasicElementDbContext : DbContext
     {
-        public DbSet<Element> Elements;
+        public DbSet<Element> Elements { get; set; }
+        public BasicElementDbContext(DbContextOptions<BasicElementDbContext> options):base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Element>().ToTable("Elements")
+                .HasKey(x => x.ID);
+        }
     }
 }
--- basic_element_database/Elements/Element.cs
+++ basic_element_database/Elements/Element.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace basic_element_database.Elements
 {
     public class Element
     {
+        [Required]
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int MaxNumberOfBound { get; set; }
+        [Required]
         public Charge Charge { get; set; }
     }
     public enum Charge
