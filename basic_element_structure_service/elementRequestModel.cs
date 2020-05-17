856f3bb002d464af8ded4ae70eb1d51e28d60d8b
dilesh <drj855@mun.ca>
2020-05-17 21:04:48 -0230

    elementresponse

---
+++ basic_element_structure_service/elementRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace basic_element_structure_service
+{
+    public class elementRequestModel
+    {
+        public string Name { get; set; }
+        public int MaxNumberOfBonds { get; set; }
+        public string Charge { get; set; }
+    }
+}
