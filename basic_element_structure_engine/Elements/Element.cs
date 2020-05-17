06a97b4019f06b290b07f2209bf1b7291fc506e2
dilesh <drj855@mun.ca>
2020-05-16 21:56:43 -0230

    init program

---
+++ basic_element_structure_engine/Elements/Element.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace basic_element_structure_engine.Elements
+{
+    public class Element
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaxNumberOfBound { get;  set; }
+        public Charge Charge { get;  set; }
+    }
+    public enum Charge
+    {
+        Zero = 0,
+        Positive,
+        Negative
+    }
+}
+
---
+++ basic_element_structure_engine/Program.cs
@@ -0,0 +1,13 @@
+﻿using basic_element_structure_engine.Elements;
+using System;
+
+namespace basic_element_structure_engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create Carbon!");
+        }
+    }
+}
