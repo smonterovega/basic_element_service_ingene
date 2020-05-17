bf9828f291181c3ab458587d173c0ba5d65ef83f
dilesh <drj855@mun.ca>
2020-05-17 15:46:21 -0230

    Adding / view data done

---
+++ basic_element_database/Respository/ElementRepository.cs
@@ -0,0 +1,14 @@
+﻿using basic_element_database.Elements;
+using basic_element_database.Repository;
+using basic_element_database.Repository.Interfaces;
+
+namespace basic_element_database.Respository
+{
+    public class ElementRepository:RepositoryBase<Element>,IRepositoryBase<Element>
+    {
+        public ElementRepository(BasicElementDbContext context):base(context)
+        {
+
+        }
+    }
+}
---
+++ basic_element_structure_service/Controllers/ElementDatabaseController.cs
@@ -0,0 +1,44 @@
+﻿using basic_element_database.Elements;
+using basic_element_database.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace basic_element_structure_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ElementDatabaseController : ControllerBase
+    {
+        private readonly IRepositoryBase<Element> _elementRepo;
+        public ElementDatabaseController(IRepositoryBase<Element> repo)
+        {
+            _elementRepo = repo;
+        }
+        [HttpGet("getallelement")]
+        public async Task<IEnumerable<Element>> getallElement() 
+        {
+            return new List<Element>
+            {
+                new Element {Name = "Carbon",MaxNumberOfBound = 4, Charge = Charge.Zero},
+                new Element{Name = "Oxygen",MaxNumberOfBound =2, Charge = Charge.Negative },
+                new Element{Name = "Nitrogen",MaxNumberOfBound =2, Charge = Charge.Negative }
+            };
+            //return await _elementRepo.FindAll();
+        }
+        [HttpPost("CreateElement")]
+        public async Task<bool> CreateElement([FromBody] Element element) 
+        {
+            try 
+            {
+                await _elementRepo.Create(element);
+                return true;
+            }
+            catch(Exception ex) 
+            {
+                return false;
+            }
+        }
+    }
+}
