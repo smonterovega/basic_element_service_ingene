39a3c08bd32f192805249e636752589bc2c14134
dilesh <drj855@mun.ca>
2020-05-17 23:13:16 -0230

    migration made visible

---
+++ basic_element_structure_service/Migrations/20200517133118_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace basic_element_structure_service.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Elements",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    MaxNumberOfBound = table.Column<int>(nullable: false),
+                    Charge = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Elements", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Elements");
+        }
+    }
+}
---
+++ basic_element_structure_service/Migrations/20200517173434_TableColumnNameChange.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace basic_element_structure_service.Migrations
+{
+    public partial class TableColumnNameChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxNumberOfBound",
+                table: "Elements");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxNumberOfBond",
+                table: "Elements",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxNumberOfBond",
+                table: "Elements");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxNumberOfBound",
+                table: "Elements",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
