6eb8f5099c14ff5a7b8e16e2e1a4b2cc24a3b161
dilesh <drj855@mun.ca>
2020-05-24 11:28:18 -0230

    project  for firebase

---
+++ basic_element_database_firebase/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace basic_element_database_firebase
+{
+    public class Class1
+    {
+        public static void Main(string[] args) 
+        {
+            
+        }
+    }
+}
